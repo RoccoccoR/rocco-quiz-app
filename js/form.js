b3564a6d0aa611c840fed50860b580210adec1d0
RoccoD <130700555+RoccoccoR@users.noreply.github.com>
2023-07-14 17:38:44 +0200

    end of the day

---
+++ js/form.js
@@ -0,0 +1,7 @@
+const submitButtonForm = document.querySelector(
+  '[data-js="form-submit-button"]'
+);
+
+submitButtonForm.addEventListener("click", () => {
+  event.preventDefault();
+});
---
+++ js/index.js
@@ -0,0 +1,25 @@
+//bookmark toggle
+
+const toggleBookmark = document.querySelector('[data-js="toggle-bookmark"]');
+
+toggleBookmark.addEventListener("click", () => {
+  toggleBookmark.classList.toggle("marked-bookmark");
+});
+
+//answer button
+
+const showAnswerButton = document.querySelector('[data-js="show-answer"]');
+const answerParagraph = document.querySelector(".main__card__answer");
+
+answerParagraph.style.display = "none";
+//Hide the answer initially
+
+showAnswerButton.addEventListener("click", () => {
+  if (answerParagraph.style.display === "none") {
+    answerParagraph.style.display = "block";
+    showAnswerButton.innerHTML = "Hide Answer";
+  } else {
+    answerParagraph.style.display = "none";
+    showAnswerButton.innerHTML = "Show Answer";
+  }
+});
